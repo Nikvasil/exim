42030e7c9dd838134b21e2a0881e6a89804dd603
Mykyta <nikvasil2001@gmail.com>
2024-06-13 01:11:45 +0200

    update configuration for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: import.meta.env.VITE_PROD_BASE_URL,
         changeOrigin: true,
         secure: false,
       },
