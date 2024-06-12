93d1ef1373e0737187f1282f49d09dad03a701db
Mykyta <nikvasil2001@gmail.com>
2024-06-12 20:46:56 +0200

    update configuration for vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': {
@@ -11,9 +15,6 @@ export default defineConfig({
         secure: false,
       },
     },
-  },
-  build: {
-    outDir: 'dist',
   }
 });
 
